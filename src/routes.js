c1966d707770f120ea0bf9e33f447ca85fba0d3f
CezarDev <bacanieskidev@gmail.com>
2024-07-14 15:44:43 -0300

    footer todo feito

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './paginas/Inicio';
 import SobreMim from './paginas/SobreMim';
 import Menu from './components/Menu';
+import Rodape from './components/Rodape';
 
 function App() {
   return (
     <BrowserRouter>
-    <Menu />
+      <Menu />
       <Routes>
         <Route path='/' element={<Inicio />} />
         <Route path='/sobreMim' element={<SobreMim />} />
         <Route path='*' element={<h1>404... Page Not Found</h1>} />
       </Routes>
+
+      <Rodape />
     </BrowserRouter>
   );
 }
