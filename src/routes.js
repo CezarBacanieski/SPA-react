aad293ae455787dc474c31a9feae5b452f186d17
CezarDev <bacanieskidev@gmail.com>
2024-07-14 15:59:20 -0300

    criacao de rotas aninhadas com react

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,18 @@ import Inicio from './paginas/Inicio';
 import SobreMim from './paginas/SobreMim';
 import Menu from './components/Menu';
 import Rodape from './components/Rodape';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 function App() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path='/sobreMim' element={<SobreMim />} />
+        <Route path='/' element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path='sobreMim' element={<SobreMim />} />
+        </Route>
+
         <Route path='*' element={<h1>404... Page Not Found</h1>} />
       </Routes>
 
